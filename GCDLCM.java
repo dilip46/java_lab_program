c9050868c2d54dcf0696f3fd1f65615538790aa2
dilip46 <dilip638196@gmail.com>
2026-07-17 12:43:33 +0530

    Create GCDLCM.java

---
+++ GCDLCM.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GCDLCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int x = a, y = b;
+
+        // Find GCD using Euclidean algorithm
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("GCD = " + gcd);
+        System.out.println("LCM = " + lcm);
+
+        sc.close();
+    }
+}
